051d9aa05847bbdf1e9483b5cf609377fd085dc8
darlingtld <darlingtld@qq.com>
2015-12-29 15:56:03 +0800

    lottery result job

--- gamble-core/src/main/java/fortune/job/LotteryResultJob.java
+++ gamble-core/src/main/java/fortune/job/LotteryResultJob.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class LotteryResultJob {
 
-    public static final int TOTAL_LOTTERY_RESULT_JOBS = 3;
+    public static final int TOTAL_LOTTERY_RESULT_JOBS = 18;
     private ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 0, TimeUnit.MINUTES, new ArrayBlockingQueue<>(TOTAL_LOTTERY_RESULT_JOBS));
 
 
@@ -30,11 +30,57 @@ public class LotteryResultJob {
 
     @Autowired
     private RuleTWOFACES ruleTWOFACES;
+    @Autowired
+    private RuleZodiacShu ruleZodiacShu;
+    @Autowired
+    private RuleZodiacNiu ruleZodiacNiu;
+    @Autowired
+    private RuleZodiacHu ruleZodiacHu;
+    @Autowired
+    private RuleZodiacTu ruleZodiacTu;
+    @Autowired
+    private RuleZodiacLong ruleZodiacLong;
+    @Autowired
+    private RuleZodiacShe ruleZodiacShe;
+    @Autowired
+    private RuleZodiacMa ruleZodiacMa;
+    @Autowired
+    private RuleZodiacYang ruleZodiacYang;
+    @Autowired
+    private RuleZodiacHou ruleZodiacHou;
+    @Autowired
+    private RuleZodiacJi ruleZodiacJi;
+    @Autowired
+    private RuleZodiacGou ruleZodiacGou;
+    @Autowired
+    private RuleZodiacZhu ruleZodiacZhu;
+    @Autowired
+    private RuleRed ruleRed;
+    @Autowired
+    private RuleGreen ruleGreen;
+    @Autowired
+    private RuleBlue ruleBlue;
+
 
     public void calculateLotteryResult() {
         Utils.logger.info("start to calculate lottery result at {}", Utils.yyyyMMddHHmmss2Format(new Date()));
         threadPoolExecutor.submit(ruleSPECIAL);
         threadPoolExecutor.submit(ruleZheng1to6);
         threadPoolExecutor.submit(ruleTWOFACES);
+        threadPoolExecutor.submit(ruleZodiacShu);
+        threadPoolExecutor.submit(ruleZodiacNiu);
+        threadPoolExecutor.submit(ruleZodiacHu);
+        threadPoolExecutor.submit(ruleZodiacTu);
+        threadPoolExecutor.submit(ruleZodiacLong);
+        threadPoolExecutor.submit(ruleZodiacShe);
+        threadPoolExecutor.submit(ruleZodiacMa);
+        threadPoolExecutor.submit(ruleZodiacYang);
+        threadPoolExecutor.submit(ruleZodiacHou);
+        threadPoolExecutor.submit(ruleZodiacJi);
+        threadPoolExecutor.submit(ruleZodiacGou);
+        threadPoolExecutor.submit(ruleZodiacZhu);
+        threadPoolExecutor.submit(ruleRed);
+        threadPoolExecutor.submit(ruleGreen);
+        threadPoolExecutor.submit(ruleBlue);
     }
 }
